bba50c1aae01144fbe3cad84d7cbebbd8bc93699
Rogerio Lima <rogerio.nunes@luizalabs.com>
2019-09-30 13:27:44 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import Config from '../src/config/environment'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('======= Dirname =====: ', __dirname)
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   await app.listen(Config.port);
 }
-bootstrap();
+
+try {
+  bootstrap();
+} catch (error) {
+  console.log('Error loading application: ', error)
+  throw error
+}
